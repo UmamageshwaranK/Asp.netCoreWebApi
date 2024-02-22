30772230c603a67111e404c3e7c82a66a8e98fa8
UmamageshwaranK <umamageshwarank6@gmail.com>
2024-02-22 10:07:09 +0530

    init

---
+++ ReactAspCrud/Models/StudentDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDBContext :DbContext
+    {
+        public StudentDBContext(DbContextOptions Options) : base(Options)
+        {
+
+        }
+        public DbSet<students> Students { get; set; }
+    }
+}
---
+++ ReactAspCrud/Models/students.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCrud.Models
+{
+    public class students
+    {
+        [Key]
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; } 
+    }
+}
