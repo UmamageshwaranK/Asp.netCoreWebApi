a6a6383aa89b5e640c3f6a56ef25f33d8e0a5023
UmamageshwaranK <umamageshwarank6@gmail.com>
2024-02-22 10:19:43 +0530

    DataBase connection

--- ReactAspCrud/Models/StudentDBContext.cs
+++ ReactAspCrud/Models/StudentDBContext.cs
@@ -10,5 +10,10 @@ namespace ReactAspCrud.Models
 
         }
         public DbSet<students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-B0CVUV9;Initial Catalog=studentDb;Integrated Security=True");
+        }
     }
 }
--- ReactAspCrud/Program.cs
+++ ReactAspCrud/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ReactAspCrud.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StudentDBContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("studentDBContext")));
+
 
 var app = builder.Build();
 
